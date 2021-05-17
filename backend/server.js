34a702175b2d17542236b311e2ec0bbc0b05a9ca
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-05-16 22:21:34 -0500

    Swapped from mongodb nodejs driver to mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,37 @@
 import express from 'express'
-import mongodb from 'mongodb'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
-/* -----------------DEV----------------- */
+/* ----------------------------------DEV---------------------------------- */
 // Load Environmental Variables
 const environmentalVars = dotenv.config({ path: './configs.env' })
-
 if (environmentalVars.error) {
     throw environmentalVars.error
 } else {
     console.log('Environmental Variables Loaded')
 }
 
-
 // Start Application
 const app = express()
 
+// Use JSON Middleware
 app.use(express.json())
 
+// Use Routes
+app.use('/account', require('./routes/accounts'))
 
 // Connect to Database
-const DBClient = new mongodb.MongoClient(process.env.DATABASE_URI, {
+mongoose.connect(process.env.DATABASE_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+    .then(() => {
+        console.log('Connected to MongoDB Database')
 
-DBClient.connect(err => {
-    if (err) { throw err }
-
-    console.log('Connected to MongoDB Database')
-
-
-    app.listen(process.env.PORT, () => {
-        console.log(`Assignment Tracker backend listening at http://localhost:${process.env.PORT}`)
+        app.listen(process.env.PORT, () => {
+            console.log(`Assignment Tracker backend listening at http://localhost:${process.env.PORT}`)
+        })
     })
-})
+    .catch(error => {
+        throw error
+    })
