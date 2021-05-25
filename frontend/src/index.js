2725dbffbb08cac1e114e36eebf1ae87f353db7c
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-05-25 00:42:01 -0500

    Added bootstrap import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux'
 import store from './app/store'
 //import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
