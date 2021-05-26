5bb9ff451ca8ba350480f7819ae02238d7e971c9
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-05-25 20:33:37 -0500

    Added bootstrap container to wrap app

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -16,6 +16,8 @@ import Dashboard from './componenets/dashboardPage/Dashboard'
 import { authenticateUser } from './actions/authActions'
 import { clearError } from './actions/errorActions'
 
+import { Container } from 'react-bootstrap'
+
 function App({ isAuthenticated, authenticateUser, clearError }) {
 
 	useEffect(() => {
@@ -26,22 +28,24 @@ function App({ isAuthenticated, authenticateUser, clearError }) {
 	}, [authenticateUser, clearError])
 
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route exact path='/login'>
-					{isAuthenticated ? <Redirect to='/dashboard' /> : <LoginPage />}
-				</Route>
-				<Route exact path='/register'>
-					{isAuthenticated ? <Redirect to='/dashboard' /> : <RegisterPage />}
-				</Route>
-				<Route exact path='/dashboard'>
-					{isAuthenticated ? <Dashboard /> : <Redirect to='/login' />}
-				</Route>
-				<Route path='/'>
-					{isAuthenticated ? <Redirect to='/dashboard' /> : <Redirect to='/login' />}
-				</Route>
-			</Switch>
-		</BrowserRouter>
+		<Container fluid className='min-vh-100 p-0 bg-dark bg-gradient' style={{ height: '1px' }}>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path='/login'>
+						{isAuthenticated ? <Redirect to='/dashboard' /> : <LoginPage />}
+					</Route>
+					<Route exact path='/register'>
+						{isAuthenticated ? <Redirect to='/dashboard' /> : <RegisterPage />}
+					</Route>
+					<Route exact path='/dashboard'>
+						{isAuthenticated ? <Dashboard /> : <Redirect to='/login' />}
+					</Route>
+					<Route path='/'>
+						{isAuthenticated ? <Redirect to='/dashboard' /> : <Redirect to='/login' />}
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</Container>
 	)
 }
 
