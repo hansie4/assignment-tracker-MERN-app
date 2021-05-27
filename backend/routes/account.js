04ef89b853dca264414da0b5e60a70b84d110b24
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-05-26 22:18:00 -0500

    Renamed to account and moved auth endpoints

---
+++ backend/routes/account.js
@@ -0,0 +1,117 @@
+const { Router } = require('express')
+const { hashSync, compareSync } = require('bcryptjs')
+const jsonwebtoken = require('jsonwebtoken')
+const auth = require('../middleware/auth')
+const Account = require('../models/account')
+
+const router = Router()
+
+/* -------------------------------------------------------- Account Information Update Endpoints */
+
+// @route   PUT account/update/email
+// @desc    Updates a user account's email
+// @access  Private
+router.put('/update/email', auth, async (req, res) => {
+    const { new_email_address } = req.body
+
+    // Checking that parameters are present
+    if (!new_email_address) return res.status(400).json({ msg: 'New email address required' })
+
+    // Query
+    await Account.findById(req.account_id, (err, doc) => {
+        if (err) {
+            return res.status(400).json({ msg: err.message })
+        } else if (doc) {
+            // Making changes
+            doc.set({ email_address: new_email_address })
+
+            // Saving the changes
+            doc.save((err) => {
+                if (err) {
+                    return res.status(400).json({ msg: err.message })
+                } else {
+                    return res.status(200).json({ msg: 'Successfully updated email address' })
+                }
+            })
+        } else {
+            return res.status(404).json({ msg: 'Could not find account document for that account id' })
+        }
+    })
+})
+
+// @route   PUT account/update/username
+// @desc    Updates a user account's username
+// @access  Private
+router.put('/update/username', auth, async (req, res) => {
+    const { new_username } = req.body
+
+    // Checking that parameters are present
+    if (!new_username) return res.status(400).json({ msg: 'New username required' })
+
+    // Query
+    await Account.findById(req.account_id, (err, doc) => {
+        if (err) {
+            return res.status(400).json({ msg: err.message })
+        } else if (doc) {
+            // Making changes
+            doc.set({ username: new_username })
+
+            // Saving the changes
+            doc.save((err) => {
+                if (err) {
+                    return res.status(400).json({ msg: err.message })
+                } else {
+                    return res.status(200).json({ msg: 'Successfully updated username' })
+                }
+            })
+        } else {
+            return res.status(404).json({ msg: 'Could not find account document for that account id' })
+        }
+    })
+})
+
+// @route   PUT account/update/password
+// @desc    Updates a user account's password
+// @access  Private
+router.put('/update/password', auth, async (req, res) => {
+    const { new_password } = req.body
+
+    // Checking that parameters are present
+    if (!new_password) return res.status(400).json({ msg: 'New password required' })
+
+    // Making sure the password is valid
+    if (!(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,256}$/.test(new_password))) {
+        return res.status(400).json({ msg: 'New password must be atleast 8 characters and contain one of each: lowercase letter, uppercase letter, number, and symbol' })
+    }
+
+    try {
+        // Encoding the password
+        const new_hashed_password = hashSync(new_password)
+        if (!new_hashed_password) throw Error('Error hashing new password')
+    } catch (error) {
+        return res.status(500).json({ msg: error.message })
+    }
+
+    // Query
+    await Account.findById(req.account_id, (err, doc) => {
+        if (err) {
+            return res.status(400).json({ msg: err.message })
+        } else if (doc) {
+            // Making changes
+            doc.set({ hashed_password: new_hashed_password })
+
+            // Saving the changes
+            doc.save((err) => {
+                if (err) {
+                    return res.status(400).json({ msg: err.message })
+                } else {
+                    return res.status(200).json({ msg: 'Successfully updated password' })
+                }
+            })
+        } else {
+            return res.status(404).json({ msg: 'Could not find account document for that account id' })
+        }
+    })
+})
+
+module.exports = router
