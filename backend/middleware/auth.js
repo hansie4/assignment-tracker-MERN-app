a348489d104afd4ef6955d6608563ebf76ddbeca
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-05-26 22:17:31 -0500

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,32 +1,23 @@
-const jwt = require('jsonwebtoken')
-
-/* ----------------------------------DEV---------------------------------- */
-// Load Environmental Variables
-if (process.env.NODE_ENV === 'development') {
-    const dotenv = require('dotenv')
-    const result = dotenv.config({ path: 'configs.env' })
-    if (result.error) {
-        throw result.error
-    }
-}
+const jsonwebtoken = require('jsonwebtoken')
 
 function auth(req, res, next) {
-
-    const token = req.header('auth-token')
-
-    // Check for token
-    if (!token) return res.status(401).json({ msg: 'No authentication token, authorization denied' })
-
     try {
+        const authHeader = req.header('Authorization')
+
+        const accessToken = authHeader.split(' ')[1]
+
+        // Check for token
+        if (!accessToken) return res.sendStatus(401)
+
         // Verify token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jsonwebtoken.verify(accessToken, process.env.JWT_ACCESS_TOKEN_SECRET)
 
         // Add decoded account id
-        req.account_id = decoded
+        req.account_id = decoded._id
 
         next()
     } catch (error) {
-        res.status(401).json({ msg: 'Token is invalid, authorization denied' })
+        return res.sendStatus(401)
     }
 }
 
