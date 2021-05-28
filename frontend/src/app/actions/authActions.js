c213fd71dcb17e8bb1f94b7d458eea5df657991d
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-05-27 20:42:46 -0500

    Updated for new auth endpoint

--- frontend/src/app/actions/authActions.js
+++ frontend/src/app/actions/authActions.js
@@ -1,224 +1,175 @@
 import axios from 'axios'
 
 import {
+    AUTH_LOADING,
+    AUTH_SUCCESS,
+    AUTH_FAILURE,
+    AUTH_REGISTER_FAILURE,
+    AUTH_REFRESH_FAILURE,
+    AUTH_LOGOUT,
+    AUTH_ERROR
+} from './actionTypes'
+
+import {
     setError,
     clearError
 } from './errorActions'
 
-export const USER_AUTH_SUCCESS = 'USER_AUTH_SUCCESS'
-export const USER_AUTH_FAILURE = 'USER_AUTH_FAILURE'
-export const USER_LOADING = 'USER_LOADING'
-export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
-export const USER_LOGIN_FAILURE = 'USER_LOGIN_FAILURE'
-export const USER_LOGOUT = 'USER_LOGOUT'
-export const USER_REGISTER_SUCCESS = 'USER_REGISTER_SUCCESS'
-export const USER_REGISTER_FAILURE = 'USER_REGISTER_FAILURE'
-export const USER_DELETE_SUCCESS = 'USER_DELETE_SUCCESS'
-export const USER_DELETE_FAILURE = 'USER_DELETE_FAILURE'
-export const USER_MODIFY_EMAIL_SUCCESS = 'USER_MODIFY_EMAIL_SUCCESS'
-export const USER_MODIFY_EMAIL_FAILURE = 'USER_MODIFY_EMAIL_FAILURE'
-export const USER_MODIFY_USERNAME_SUCCESS = 'USER_MODIFY_USERNAME_SUCCESS'
-export const USER_MODIFY_USERNAME_FAILURE = 'USER_MODIFY_USERNAME_FAILURE'
-export const USER_MODIFY_PASSWORD_SUCCESS = 'USER_MODIFY_PASSWORD_SUCCESS'
-export const USER_MODIFY_PASSWORD_FAILURE = 'USER_MODIFY_PASSWORD_FAILURE'
-
-export const authenticateUser = () => async (dispatch, getState) => {
-    dispatch({ type: USER_LOADING })
-
-    const url = '/account/'
-    const body = {}
-    const config = tokenConfig(getState)
-
-    await axios.post(url, body, config)
-        .then(() => {
-            dispatch(clearError())
-            dispatch({ type: USER_AUTH_SUCCESS })
-        })
-        .catch((error) => {
-            if (error.response) {
-                dispatch(setError(error.response.status, error.response.data.msg))
-            } else if (error.request) {
-                dispatch(setError(500, 'No respose recieved from the server'))
-            } else {
-                dispatch(setError(400, 'Error creating request'))
-            }
-            dispatch({ type: USER_AUTH_FAILURE })
-        })
-}
-
 export const loginUser = (username, password) => async (dispatch, getState) => {
-    dispatch({ type: USER_LOADING })
+    dispatch({ type: AUTH_LOADING })
 
-    const url = '/account/login'
+    const url = '/auth/login'
     const body = {
         username,
         password
     }
-    const config = tokenConfig(getState)
+    const config = authTokenConfig(getState)
 
     await axios.post(url, body, config)
         .then((response) => {
             dispatch(clearError())
             dispatch({
-                type: USER_LOGIN_SUCCESS,
+                type: AUTH_SUCCESS,
                 payload: {
-                    authToken: response.data.token
+                    accessToken: response.data.access_token
                 }
             })
         })
         .catch((error) => {
             if (error.response) {
-                dispatch(setError(error.response.status, error.response.data.msg))
+                if (error.response.status === 401) {
+                    dispatch(setError(error.response.status, 'Invalid credentials'))
+                } else {
+                    dispatch(setError(error.response.status, error.response.data.msg))
+                }
+                dispatch({ type: AUTH_FAILURE })
             } else if (error.request) {
-                dispatch(setError(500, 'No respose recieved from the server'))
+                dispatch(setError(500, 'No response from authentication server'))
+                dispatch({ type: AUTH_ERROR })
             } else {
-                dispatch(setError(400, 'Error creating request'))
+                dispatch(setError(400, 'Error creating login request'))
+                dispatch({ type: AUTH_ERROR })
             }
-            dispatch({ type: USER_LOGIN_FAILURE })
         })
 }
 
 export const registerUser = (email_address, username, password) => async (dispatch, getState) => {
-    dispatch({ type: USER_LOADING })
+    dispatch({ type: AUTH_LOADING })
 
-    const url = '/account/register'
+    const url = '/auth/register'
     const body = {
         email_address,
         username,
         password
     }
-    const config = tokenConfig(getState)
+    const config = authTokenConfig(getState)
 
     await axios.post(url, body, config)
         .then((response) => {
             dispatch(clearError())
             dispatch({
-                type: USER_REGISTER_SUCCESS,
+                type: AUTH_SUCCESS,
                 payload: {
-                    authToken: response.data.token
+                    accessToken: response.data.access_token
                 }
             })
         })
         .catch((error) => {
             if (error.response) {
                 dispatch(setError(error.response.status, error.response.data.msg))
+                dispatch({ type: AUTH_REGISTER_FAILURE })
             } else if (error.request) {
-                dispatch(setError(500, 'No respose recieved from the server'))
+                dispatch(setError(500, 'No response from authentication server'))
+                dispatch({ type: AUTH_ERROR })
             } else {
-                dispatch(setError(400, 'Error creating request'))
+                dispatch(setError(400, 'Error creating register request'))
+                dispatch({ type: AUTH_ERROR })
             }
-            dispatch({ type: USER_REGISTER_FAILURE })
         })
 }
 
-export const deleteUser = () => async (dispatch, getState) => {
-    dispatch({ type: USER_LOADING })
+export const refreshToken = () => async (dispatch, getState) => {
+    dispatch({ type: AUTH_LOADING })
 
-    const url = '/account/delete'
-    const config = tokenConfig(getState)
+    const url = '/auth/refresh'
+    const config = authTokenConfig(getState)
+
+    await axios.get(url, config)
+        .then((response) => {
+            dispatch(clearError())
+            dispatch({
+                type: AUTH_SUCCESS,
+                payload: {
+                    accessToken: response.data.access_token
+                }
+            })
+        })
+        .catch((error) => {
+            if (error.response) {
+                dispatch(setError(error.response.status, error.response.data.msg))
+                dispatch({ type: AUTH_REFRESH_FAILURE })
+            } else if (error.request) {
+                dispatch(setError(500, 'No response from authentication server'))
+                dispatch({ type: AUTH_ERROR })
+            } else {
+                dispatch(setError(400, 'Error access token refresh request'))
+                dispatch({ type: AUTH_ERROR })
+            }
+        })
+}
+
+export const logoutUser = () => async (dispatch, getState) => {
+    dispatch({ type: AUTH_LOADING })
+
+    const url = '/auth/logout'
+    const body = {}
+    const config = authTokenConfig(getState)
+
+    await axios.post(url, body, config)
+        .then(() => {
+            dispatch(clearError())
+            dispatch({ type: AUTH_LOGOUT })
+        })
+        .catch((error) => {
+            if (error.response) {
+                dispatch(setError(error.response.status, error.response.data.msg))
+                dispatch({ type: AUTH_LOGOUT })
+            } else if (error.request) {
+                dispatch(setError(500, 'No response from authentication server'))
+                dispatch({ type: AUTH_ERROR })
+            } else {
+                dispatch(setError(400, 'Error creating logout request'))
+                dispatch({ type: AUTH_ERROR })
+            }
+        })
+}
+
+export const logoutAllUserDevices = () => async (dispatch, getState) => {
+    dispatch({ type: AUTH_LOADING })
+
+    const url = '/auth/logout_all'
+    const config = authTokenConfig(getState)
 
     await axios.delete(url, config)
         .then(() => {
             dispatch(clearError())
-            dispatch({ type: USER_DELETE_SUCCESS })
+            dispatch({ type: AUTH_LOGOUT })
         })
         .catch((error) => {
             if (error.response) {
                 dispatch(setError(error.response.status, error.response.data.msg))
+                dispatch({ type: AUTH_LOGOUT })
             } else if (error.request) {
-                dispatch(setError(500, 'No respose recieved from the server'))
+                dispatch(setError(500, 'No response from authentication server'))
             } else {
-                dispatch(setError(400, 'Error creating request'))
+                dispatch(setError(400, 'Error creating logout request'))
             }
-            dispatch({ type: USER_DELETE_FAILURE })
+            dispatch({ type: AUTH_ERROR })
         })
 }
 
-export const modifyEmailAddress = (new_email_address) => async (dispatch, getState) => {
-    dispatch({ type: USER_LOADING })
-
-    const url = '/account/update/email'
-    const body = {
-        new_email_address
-    }
-    const config = tokenConfig(getState)
-
-    await axios.post(url, body, config)
-        .then(() => {
-            dispatch(clearError())
-            dispatch({ type: USER_MODIFY_EMAIL_SUCCESS })
-        })
-        .catch((error) => {
-            if (error.response) {
-                dispatch(setError(error.response.status, error.response.data.msg))
-            } else if (error.request) {
-                dispatch(setError(500, 'No respose recieved from the server'))
-            } else {
-                dispatch(setError(400, 'Error creating request'))
-            }
-            dispatch({ type: USER_MODIFY_EMAIL_FAILURE })
-        })
-}
-
-export const modifyUsername = (new_username) => async (dispatch, getState) => {
-    dispatch({ type: USER_LOADING })
-
-    const url = '/account/update/username'
-    const body = {
-        new_username
-    }
-    const config = tokenConfig(getState)
-
-    await axios.post(url, body, config)
-        .then(() => {
-            dispatch(clearError())
-            dispatch({ type: USER_MODIFY_USERNAME_SUCCESS })
-        })
-        .catch((error) => {
-            if (error.response) {
-                dispatch(setError(error.response.status, error.response.data.msg))
-            } else if (error.request) {
-                dispatch(setError(500, 'No respose recieved from the server'))
-            } else {
-                dispatch(setError(400, 'Error creating request'))
-            }
-            dispatch({ type: USER_MODIFY_USERNAME_FAILURE })
-        })
-}
-
-export const modifyPassword = (new_password) => async (dispatch, getState) => {
-    dispatch({ type: USER_LOADING })
-
-    const url = '/account/update/password'
-    const body = {
-        new_password
-    }
-    const config = tokenConfig(getState)
-
-    await axios.post(url, body, config)
-        .then(() => {
-            dispatch(clearError())
-            dispatch({ type: USER_MODIFY_PASSWORD_SUCCESS })
-        })
-        .catch((error) => {
-            if (error.response) {
-                dispatch(setError(error.response.status, error.response.data.msg))
-            } else if (error.request) {
-                dispatch(setError(500, 'No respose recieved from the server'))
-            } else {
-                dispatch(setError(400, 'Error creating request'))
-            }
-            dispatch({ type: USER_MODIFY_PASSWORD_FAILURE })
-        })
-}
-
-export const logoutUser = () => (dispatch) => {
-    dispatch({ type: USER_LOADING })
-    dispatch({ type: USER_LOGOUT })
-}
-
-export const tokenConfig = getState => {
-    const token = getState().auth.authToken
+export const authTokenConfig = getState => {
+    const token = getState().auth.accessToken
 
     const config = {
         headers: {
@@ -227,7 +178,7 @@ export const tokenConfig = getState => {
         timeout: 1000
     }
 
-    if (token) config.headers['auth-token'] = token
+    if (token) config.headers['Authorization'] = 'Bearer '.concat(token)
 
     return config
 }
