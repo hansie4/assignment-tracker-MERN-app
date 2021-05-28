803aafff2dedfacbd3c2aac71ce64cf2e68ea633
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-05-27 20:41:46 -0500

    Moved action types to its own file

---
+++ frontend/src/app/actions/actionTypes.js
@@ -0,0 +1,14 @@
+// --------------------------------- ERROR Action Types ---------------------------------
+
+export const SET_ERROR = 'SET_ERROR'
+export const CLEAR_ERROR = 'CLEAR_ERROR'
+
+// --------------------------------- AUTH Action Types ---------------------------------
+
+export const AUTH_LOADING = 'AUTH_LOADING'
+export const AUTH_SUCCESS = 'AUTH_SUCCESS'
+export const AUTH_FAILURE = 'AUTH_FAILURE'
+export const AUTH_REGISTER_FAILURE = 'AUTH_REGISTER_FAILURE'
+export const AUTH_REFRESH_FAILURE = 'AUTH_REFRESH_FAILURE'
+export const AUTH_LOGOUT = 'AUTH_LOGOUT'
+export const AUTH_ERROR = 'AUTH_ERROR'
