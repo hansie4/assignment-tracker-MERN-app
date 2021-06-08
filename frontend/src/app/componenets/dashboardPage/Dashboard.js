42eba5670f506f37a23fa8eabc64ef5a8dba219d
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-06-08 14:31:26 -0500

    Update Dashboard.js

--- frontend/src/app/componenets/dashboardPage/Dashboard.js
+++ frontend/src/app/componenets/dashboardPage/Dashboard.js
@@ -4,13 +4,7 @@ import { useHistory } from 'react-router-dom'
 
 import { connect } from 'react-redux'
 
-import {
-    getAccountInfo,
-    logoutUser,
-    logoutUserAllDevices
-} from '../../actions/accountActions'
-
-import { refreshToken } from '../../actions/authActions'
+import { getAccountInfo } from '../../actions/accountActions'
 
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -22,13 +16,9 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import { PersonCircle, BoxArrowRight } from 'react-bootstrap-icons'
 
 function Dashboard({
-    auth,
     error,
-    account,
-    refreshToken,
     getAccountInfo,
-    logoutUser,
-    logoutUserAllDevices
+    logoutUser
 }) {
 
     let history = useHistory()
@@ -56,30 +46,13 @@ function Dashboard({
             </Navbar>
             <Container fluid>
                 <Row className='p-2 justify-content-center'>
-                    <Col className='bg-light rounded-3'>
-                        <h1>DASHBOARD</h1>
-                        <div className='text-break'>
-                            {JSON.stringify(auth)}
-                        </div>
-                        <div className='text-break'>
-                            {JSON.stringify(error)}
-                        </div>
-                        <div className='text-break'>
-                            {JSON.stringify(account)}
-                        </div>
-                        <Button onClick={() => logoutUserAllDevices()}>LOGOUT ALL</Button>
-                        <Button onClick={() => refresh(refreshToken)}>REFRESH</Button>
+                    <Col className='p-2 bg-light rounded-3' style={{ minHeight: '92vh' }}>
+
                     </Col>
                 </Row>
             </Container>
         </Container>
-
     )
-}
-
-const refresh = async (fun) => {
-    const test = await fun()
-    console.log(test)
 }
 
 const redirectToAccountPage = (history) => {
@@ -87,16 +60,11 @@ const redirectToAccountPage = (history) => {
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    error: state.error,
-    account: state.account
+    error: state.error
 })
 
 const mapDispatchToProps = {
-    refreshToken,
-    getAccountInfo,
-    logoutUser,
-    logoutUserAllDevices
+    getAccountInfo
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
