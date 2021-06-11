fcf6fdc62c4b1eb200a8f238cabb009f1d20350b
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-06-10 21:07:16 -0500

    Added error toasts

--- frontend/src/app/componenets/dashboardPage/Dashboard.js
+++ frontend/src/app/componenets/dashboardPage/Dashboard.js
@@ -1,10 +1,12 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 import { useHistory } from 'react-router-dom'
 
 import { connect } from 'react-redux'
 
-import { getAccountInfo } from '../../actions/accountActions'
+import { logoutUser } from '../../actions/accountActions'
+import { getTrackerInfo } from '../../actions/trackerActions'
+import { clearError } from '../../actions/errorActions'
 
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -12,20 +14,33 @@ import Col from 'react-bootstrap/Col'
 import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Toast from 'react-bootstrap/Toast'
 
 import { PersonCircle, BoxArrowRight } from 'react-bootstrap-icons'
 
+import TrackerDisplay from './dashboardComponenets/TrackerDisplay'
+
 function Dashboard({
     error,
-    getAccountInfo,
-    logoutUser
+    getTrackerInfo,
+    logoutUser,
+    clearError
 }) {
 
     let history = useHistory()
 
+    const [showError, setShowError] = useState(false)
+
     useEffect(() => {
-        getAccountInfo()
-    }, [getAccountInfo])
+        getTrackerInfo()
+    }, [getTrackerInfo])
+
+    useEffect(() => {
+        error ?
+            setShowError(true)
+            :
+            setShowError(false)
+    }, [error])
 
     return (
         <Container fluid className='p-0'>
@@ -33,7 +48,7 @@ function Dashboard({
                 <Container fluid className='d-flex justify-content-between'>
                     <Navbar.Brand href='/dashboard'>Assignment Tracker</Navbar.Brand>
                     <ButtonGroup>
-                        <Button variant='outline-light' onClick={() => redirectToAccountPage(history)}>
+                        <Button variant='outline-light' onClick={() => redirectToAccountPage(history, clearError)}>
                             Account
                             <PersonCircle className='ms-2' />
                         </Button>
@@ -46,8 +61,24 @@ function Dashboard({
             </Navbar>
             <Container fluid>
                 <Row className='p-2 justify-content-center'>
-                    <Col className='p-2 bg-light rounded-3' style={{ minHeight: '92vh' }}>
+                    <Col className='p-0 bg-light rounded-3' style={{ minHeight: '93vh' }}>
 
+                        <TrackerDisplay />
+
+                        <Toast
+                            className="m-3 bg-warning"
+                            show={showError}
+                            onClose={() => {
+                                setShowError(false); clearError()
+                            }}
+                            style={{ position: 'fixed', bottom: 0, right: 0 }}
+                            animation={false}
+                        >
+                            <Toast.Header>
+                                <strong className="mr-auto">Error</strong>
+                            </Toast.Header>
+                            <Toast.Body>{error ? error.message : null}</Toast.Body>
+                        </Toast>
                     </Col>
                 </Row>
             </Container>
@@ -55,16 +86,19 @@ function Dashboard({
     )
 }
 
-const redirectToAccountPage = (history) => {
+const redirectToAccountPage = (history, clearErrorMethod) => {
+    clearErrorMethod()
     history.push('/account')
 }
 
 const mapStateToProps = state => ({
-    error: state.error
+    error: state.error.error
 })
 
 const mapDispatchToProps = {
-    getAccountInfo
+    getTrackerInfo,
+    logoutUser,
+    clearError
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
