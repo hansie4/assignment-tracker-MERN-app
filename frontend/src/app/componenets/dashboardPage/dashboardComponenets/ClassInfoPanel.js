8b76231b2e4b95710e82f13371482ab71182294c
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-06-16 00:44:52 -0500

    Moved class info to props

--- frontend/src/app/componenets/dashboardPage/dashboardComponenets/ClassInfoPanel.js
+++ frontend/src/app/componenets/dashboardPage/dashboardComponenets/ClassInfoPanel.js
@@ -1,12 +1,8 @@
-import {
-    useState,
-    useEffect
-} from 'react'
+import { useState } from 'react'
 
 import { connect } from 'react-redux'
 
 import {
-    getSelectedClassInfo,
     modifyClass,
     deleteClass
 } from '../../../actions/trackerActions'
@@ -20,46 +16,39 @@ import FormControl from 'react-bootstrap/FormControl'
 import { GearFill, TrashFill, XCircleFill, CheckCircleFill } from 'react-bootstrap-icons'
 
 function ClassInfoPanel({
-    getSelectedClassInfo,
+    selectedClass,
     modifyClass,
     deleteClass,
-    selected_semester_id,
-    selected_class_id
+    selected_semester_id
 }) {
 
-    const [class_, setClass] = useState(null)
     const [modifyMode, setModifyMode] = useState(false)
 
     const [new_name, setNewName] = useState(null)
     const [new_description, setNewDescription] = useState(null)
 
-    useEffect(() => {
-        setClass(getSelectedClassInfo())
-        setModifyMode(false)
-    }, [getSelectedClassInfo, selected_class_id, selected_semester_id])
-
-
-    if (class_) {
+    if (selectedClass) {
         return (
             <Card className='border border-dark'>
                 <Card.Header className='d-flex justify-content-between p-1 ps-3'>
-                    {
-                        modifyMode ?
-                            <InputGroup>
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text>New Class Name:</InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl placeholder={class_.name} onChange={(event) => handleChange(event, setNewName)} />
-                            </InputGroup>
-                            :
-                            <h3>{class_.name}</h3>
-                    }
+                    <h3>{selectedClass.name}</h3>
                     <ButtonGroup size="sm">
                         <Button variant='secondary' onClick={() => setModifyMode(!modifyMode)}><GearFill /></Button>
-                        <Button variant='danger' onClick={() => deleteClass({ class_id: selected_class_id, semester_id: selected_semester_id })}><TrashFill /></Button>
+                        <Button variant='danger' onClick={() => deleteClass({ class_id: selectedClass._id, semester_id: selected_semester_id })}><TrashFill /></Button>
                     </ButtonGroup>
                 </Card.Header>
                 <Card.Body>
+                    {
+                        modifyMode ?
+                            <InputGroup className='mb-3'>
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text>New Class Name:</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl placeholder={selectedClass.name} onChange={(event) => handleChange(event, setNewName)} />
+                            </InputGroup>
+                            :
+                            null
+                    }
                     <Card.Title>Description:</Card.Title>
                     {
                         modifyMode ?
@@ -67,10 +56,10 @@ function ClassInfoPanel({
                                 <InputGroup.Prepend>
                                     <InputGroup.Text>New Description:</InputGroup.Text>
                                 </InputGroup.Prepend>
-                                <FormControl placeholder={class_.description} onChange={(event) => handleChange(event, setNewDescription)} />
+                                <FormControl placeholder={selectedClass.description} onChange={(event) => handleChange(event, setNewDescription)} />
                             </InputGroup>
                             :
-                            <Card.Text>{class_.description}</Card.Text>
+                            <Card.Text>{selectedClass.description}</Card.Text>
                     }
                 </Card.Body>
                 {
@@ -78,7 +67,7 @@ function ClassInfoPanel({
                         <Card.Footer>
                             <ButtonGroup className='w-100'>
                                 <Button variant='secondary' onClick={() => setModifyMode(false)}><XCircleFill /></Button>
-                                <Button variant='success' onClick={() => modifyClass({ class_id: selected_class_id, semester_id: selected_semester_id, new_name, new_description })}><CheckCircleFill /></Button>
+                                <Button variant='success' onClick={() => modifyClass({ class_id: selectedClass._id, semester_id: selected_semester_id, new_name, new_description })}><CheckCircleFill /></Button>
                             </ButtonGroup>
                         </Card.Footer>
                         :
@@ -98,12 +87,10 @@ const handleChange = (event, setMethod) => {
 }
 
 const mapStateToProps = state => ({
-    selected_semester_id: state.tracker.selected_semester_id,
-    selected_class_id: state.tracker.selected_class_id
+    selected_semester_id: state.tracker.selected_semester_id
 })
 
 const mapDispatchToProps = {
-    getSelectedClassInfo,
     modifyClass,
     deleteClass
 }
