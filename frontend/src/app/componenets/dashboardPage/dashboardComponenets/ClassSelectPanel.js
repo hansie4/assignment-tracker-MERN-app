8ac18ddd42e4e49360d03cc90d800ff3ae3c36e3
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-06-16 00:44:34 -0500

    Now gets classes from props

--- frontend/src/app/componenets/dashboardPage/dashboardComponenets/ClassSelectPanel.js
+++ frontend/src/app/componenets/dashboardPage/dashboardComponenets/ClassSelectPanel.js
@@ -1,15 +1,10 @@
-import {
-    useState,
-    useEffect
-} from 'react'
+import { useState } from 'react'
 
 import { connect } from 'react-redux'
 
-import {
-    getSelectedSemesterInfo,
-    selectClass
-} from '../../../actions/trackerActions'
+import { selectClass } from '../../../actions/trackerActions'
 
+import Card from 'react-bootstrap/Card'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Button from 'react-bootstrap/Button'
 
@@ -18,56 +13,53 @@ import { PlusCircle } from 'react-bootstrap-icons'
 import AddClassModal from './modals/AddClassModal'
 
 function ClassSelectPanel({
-    getSelectedSemesterInfo,
+    classes,
     selectClass,
-    selected_semester_id,
     selected_class_id
 }) {
 
-    const [classes, setClasses] = useState(null)
     const [addClassModalShown, setAddClassModalShown] = useState(false)
 
-    useEffect(() => {
-        setClasses(getSelectedSemesterInfo().classes)
-    }, [getSelectedSemesterInfo, selected_semester_id])
 
     return (
         <div>
             <AddClassModal show={addClassModalShown} close={() => setAddClassModalShown(false)} />
-            <div className='w-100 border border-dark rounded bg-light'>
-                <h5 className='text-center pt-2 m-0 pb-2 border-bottom border-dark rounded'>Classes:</h5>
-                <ButtonGroup vertical className='w-100'>
-                    {
-                        classes ?
-                            classes.map((current_class, index) => {
-                                return (
-                                    <Button
-                                        key={index}
-                                        onClick={() => selectClass(current_class._id)}
-                                        disabled={current_class._id === selected_class_id}
-                                        className='w-100'
-                                    >
-                                        {current_class.name}
-                                    </Button>
-                                )
-                            })
-                            :
-                            null
-                    }
-                    <Button variant='light' className='border-top border-dark' onClick={() => setAddClassModalShown(true)}><PlusCircle /></Button>
-                </ButtonGroup>
-            </div>
+            <Card className='border border-dark'>
+                <Card.Header>
+                    <Card.Title className='m-0'>Classes:</Card.Title>
+                </Card.Header>
+                <Card.Body className='p-0'>
+                    <ButtonGroup vertical className='w-100'>
+                        {
+                            classes ?
+                                classes.map((current_class, index) => {
+                                    return (
+                                        <Button
+                                            key={index}
+                                            onClick={() => selectClass(current_class._id)}
+                                            disabled={current_class._id === selected_class_id}
+                                            className='w-100'
+                                        >
+                                            {current_class.name}
+                                        </Button>
+                                    )
+                                })
+                                :
+                                null
+                        }
+                        <Button variant='light' onClick={() => setAddClassModalShown(true)}><PlusCircle /></Button>
+                    </ButtonGroup>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
 
 const mapStateToProps = state => ({
-    selected_semester_id: state.tracker.selected_semester_id,
     selected_class_id: state.tracker.selected_class_id
 })
 
 const mapDispatchToProps = {
-    getSelectedSemesterInfo,
     selectClass
 }
 
