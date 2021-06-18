c60ca9a7b90737ed2ad00d5712cf325dcf94d893
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-06-17 23:38:34 -0500

    Dev tools bugfix

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -9,10 +9,20 @@ import rootReducer from './reducers/rootReducer'
 
 const initialState = {};
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(thunk), ((process.env.NODE_ENV === 'production') ? null : (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))),
-)
+let store
+
+if (process.env.NODE_ENV === 'production') {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(applyMiddleware(thunk)),
+    )
+} else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
+    )
+}
 
 export default store
