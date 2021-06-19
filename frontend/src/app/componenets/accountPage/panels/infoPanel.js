1c128dbeb5b86cdff4692297129e2f9a412d2e05
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-06-19 12:31:18 -0500

    Update infoPanel.js

--- frontend/src/app/componenets/accountPage/panels/infoPanel.js
+++ frontend/src/app/componenets/accountPage/panels/infoPanel.js
@@ -12,7 +12,8 @@ function InfoPanel(props) {
     )
 }
 
-const formatDate = (date) => {
+const formatDate = (dateString) => {
+    const date = new Date(dateString)
     const month = new Intl.DateTimeFormat('en', { month: 'short' }).format(date)
     const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date)
     const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date)
