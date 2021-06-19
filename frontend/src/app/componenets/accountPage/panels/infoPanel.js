fe0f3d086fefd7917d694e95fd827bddd34e28ca
Hans Von Gruenigen <64754614+hansie4@users.noreply.github.com>
2021-06-18 22:53:49 -0500

    Register date now formated

--- frontend/src/app/componenets/accountPage/panels/infoPanel.js
+++ frontend/src/app/componenets/accountPage/panels/infoPanel.js
@@ -7,9 +7,16 @@ function InfoPanel(props) {
             <hr />
             <p className="fs-4">Username: {props.username}</p>
             <p className="fs-4">Email Address: {props.email_address}</p>
-            <p className="fs-4">Date Registered: {new Date(props.date_registered)}</p>
+            <p className="fs-4">Date Registered: {formatDate(props.date_registered)}</p>
         </div>
     )
+}
+
+const formatDate = (date) => {
+    const month = new Intl.DateTimeFormat('en', { month: 'short' }).format(date)
+    const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date)
+    const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date)
+    return (`${month}-${day}-${year}`)
 }
 
 const mapStateToProps = state => ({
